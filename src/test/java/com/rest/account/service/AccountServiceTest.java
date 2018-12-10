50700e24d612c609026653f88738e2751a5c9929
Roshini <roshinirajesh1@gmail.com>
2018-12-10 22:49:57 +0000

    Added service test for Post endpoint.

--- src/test/java/com/rest/account/service/AccountServiceTest.java
+++ src/test/java/com/rest/account/service/AccountServiceTest.java
@@ -1,24 +1,26 @@
 package com.rest.account.service;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.rest.account.entity.AccountEntity;
 import com.rest.account.mapper.AccountMapper;
 import com.rest.account.model.AccountDto;
+import com.rest.account.model.MessageDto;
 import com.rest.account.repository.AccountRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 @RunWith(MockitoJUnitRunner.class)
 public class AccountServiceTest {
+    private static final String ACCOUNT_ADDED = "account has been successfully added";
 
     @Mock
     private AccountRepository accountRepository;
@@ -44,5 +46,18 @@ public class AccountServiceTest {
 
         final List<AccountDto> actual = accountService.getAccounts();
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void saveAccountTest() {
+
+        final AccountDto accountDto = mock(AccountDto.class);
+        final AccountEntity accountEntity = mock(AccountEntity.class);
+
+        when(accountMapper.toAccountEntity(accountDto)).thenReturn(accountEntity);
+        final MessageDto actual = accountService.saveAccount(accountDto);
+
+        assertThat(actual).isEqualTo(new MessageDto(ACCOUNT_ADDED));
+        verify(accountRepository).save(accountEntity);
     }
 }
